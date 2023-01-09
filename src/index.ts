5c9c81e62b9f70cac9f749db3affb40c049bda47
Umut Aydin <aydinumut82@gmail.com>
2023-01-09 20:38:51 +0300

    presets removed

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import { ContainerImage } from 'aws-cdk-lib/aws-ecs'
 import { FileSystem } from 'aws-cdk-lib/aws-efs'
 import { HostedZone } from 'aws-cdk-lib/aws-route53'
 import { InstanceClass, InstanceSize, InstanceType, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2'
-import { Presets } from './utils/presets'
 import { CfnOutput, RemovalPolicy } from 'aws-cdk-lib'
 import { Secret } from 'aws-cdk-lib/aws-secretsmanager'
 
@@ -31,22 +30,23 @@ export class Wordpress extends Construct {
         const volume = 'WordpressVolume-' + id
         const containerPath = '/bitnami/wordpress'
 
-        const preset = props.preset || Preset.BASIC
-        const maximumAvailabilityZones =
-            props.advanced?.maximumAvailabilityZones || Presets[preset].maximumAvailabilityZones
-        const natGateways = props.advanced?.natGateways || Presets[preset].natGateways
-        const taskCPU = props.advanced?.taskCPU || Presets[preset].taskCPU
-        const taskMemory = props.advanced?.taskMemory || Presets[preset].taskMemory
-        const desiredTaskInstanceCount =
-            props.advanced?.desiredTaskInstanceCount || Presets[preset].desiredTaskInstanceCount
-        const cpuThreshold = props.advanced?.cpuThreshold || Presets[preset].cpuThreshold
-        const memoryThreshold = props.advanced?.memoryThreshold || Presets[preset].memoryThreshold
-        const minCapacity = props.advanced?.minCapacity || Presets[preset].minCapacity
-        const maxCapacity = props.advanced?.maxCapacity || Presets[preset].maxCapacity
+        const maximumAvailabilityZones = props.advanced?.maximumAvailabilityZones || 1
+        const natGateways = props.advanced?.natGateways || 1
+        const taskCPU = props.advanced?.taskCPU || 256
+        const taskMemory = props.advanced?.taskMemory || 1024
+        const desiredTaskInstanceCount = props.advanced?.desiredTaskInstanceCount || 1
+        const cpuThreshold = props.advanced?.cpuThreshold || 85
+        const memoryThreshold = props.advanced?.memoryThreshold || 85
+        const minCapacity = props.advanced?.minCapacity || 1
+        const maxCapacity = props.advanced?.maxCapacity || 1
 
         const vpc = new Vpc(this, 'WordpressVPC-' + id, {
             maxAzs: maximumAvailabilityZones,
             natGateways,
+            subnetConfiguration: [
+                { name: 'public', subnetType: SubnetType.PUBLIC, cidrMask: 24 },
+                { name: 'privateIsolated', subnetType: SubnetType.PRIVATE_ISOLATED, cidrMask: 24 },
+            ]
         })
         const secretDB = new Secret(this, 'WordpressDBSecret-' + id, {
             secretName: `password-of-${databaseUsername}`,
@@ -91,8 +91,8 @@ export class Wordpress extends Construct {
             taskSubnets: { subnetType: SubnetType.PRIVATE_ISOLATED },
             desiredCount: desiredTaskInstanceCount,
             publicLoadBalancer: true,
-            domainName,
-            domainZone: domainName ? HostedZone.fromLookup(this, 'WordpressDomain-' + id, { domainName }) : undefined,
+            // domainName,
+            // domainZone: domainName ? HostedZone.fromLookup(this, 'WordpressDomain-' + id, { domainName }) : undefined,
         })
 
         db.connections.allowDefaultPortFrom(wp.service.connections)
@@ -114,9 +114,6 @@ export class Wordpress extends Construct {
 
         const dbHostOutput = 'WordpressDBHost-' + id
         new CfnOutput(this, dbHostOutput, { exportName: dbHostOutput, value: db.clusterEndpoint.hostname })
-
-        const dbPasswordOutput = 'WordpressDBPassword-' + id
-        new CfnOutput(this, dbPasswordOutput, { exportName: dbPasswordOutput, value: secretDB.secretValue.toString() })
 
         const wpPasswordOutput = 'WordpressPassword-' + id
         new CfnOutput(this, wpPasswordOutput, { exportName: wpPasswordOutput, value: secretWP.secretValue.toString() })
